63f636a0fa1ce9aefb2b9c93042d288528dd14d6
Lokesh <lok3sh2007@gmail.com>
2026-02-19 22:39:15 +0530

    YoLO

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const uptimeHours = Math.floor(osInformation.uptime / 3600);
 const uptimeMinutes = Math.floor((osInformation.uptime % 3600) / 60);
 
 
-console.log(chalk.blueBright(`${osInformation.userName}`,`${chalk.blueBright(`${osInformation.host}`)}`));
+console.log(chalk.blueBright.bold(`${osInformation.userName}`,`${chalk.blueBright.bold(`${osInformation.host}`)}`));
 
 console.log(chalk.blueBright(`------------------------------------------------------------------------------`));
 dis();
-console.log(`${chalk.redBright('OS:')} ${osInformation.osVersion}`);
-console.log(`${chalk.redBright('Host:')} ${osInformation.host}`);
-console.log(`${chalk.redBright('Kernel:')} ${osInformation.kernel}`);
-console.log(`${chalk.redBright('Terminal:')} ${env.terminal}`);
-console.log(`${chalk.redBright('Shell:')} ${env.shell}`);
-console.log(`${chalk.redBright('CPU:')} ${osInformation.cpuName.trim()} (${osInformation.cpuCores}) @ ${osInformation.cpucoreSpeed}GHz`);
-console.log(`${chalk.redBright('Used Memory:')} ${(osInformation.totalMemoryGb - osInformation.freeMemoryGb).toFixed(2)}/${osInformation.totalMemoryGb} GB`);
-console.log(`${chalk.redBright('Uptime:')} ${uptimeHours} hours ${uptimeMinutes} minutes`);
-console.log(`${chalk.redBright('Node version:')} ${osInformation.nodeVersion}`);
+console.log(`${chalk.redBright.bold('OS:')} ${chalk.rgb(255, 173, 188)(`${osInformation.osVersion}`)}`);
+console.log(`${chalk.redBright.bold('Host:')} ${osInformation.host}`);
+console.log(`${chalk.redBright.bold('Kernel:')} ${osInformation.kernel}`);
+console.log(`${chalk.redBright.bold('Terminal:')} ${env.terminal}`);
+console.log(`${chalk.redBright.bold('Shell:')} ${env.shell}`);
+console.log(`${chalk.redBright.bold('CPU:')} ${osInformation.cpuName.trim()} (${osInformation.cpuCores}) @ ${osInformation.cpucoreSpeed}GHz`);
+console.log(`${chalk.redBright.bold('Used Memory:')} ${(osInformation.totalMemoryGb - osInformation.freeMemoryGb).toFixed(2)}/${osInformation.totalMemoryGb} GB`);
+console.log(`${chalk.redBright.bold('Uptime:')} ${uptimeHours} hours ${uptimeMinutes} minutes`);
+console.log(`${chalk.redBright.bold('Node version:')} ${osInformation.nodeVersion}`);
 
 async function dis() {
   const displayInfo = await getInfo();
-  console.log(`${chalk.redBright('GPU:')} ${displayInfo.model}`);
-  console.log(`${chalk.redBright('Resolution:')} ${displayInfo.resolutionX} X ${displayInfo.resolutionY}`)
+  console.log(`${chalk.redBright.bold('GPU:')} ${displayInfo.model}`);
+  console.log(`${chalk.redBright.bold('Resolution:')} ${displayInfo.resolutionX} X ${displayInfo.resolutionY}`)
 }
